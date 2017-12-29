f717ff2901fd2e8bfd6d09e7156bee67097f9339
Tomoaki Tsuruta <england0112@gmail.com>
2017-12-29 15:28:57 +0900

    Create seed files

--- cassandra/setup.sh
+++ cassandra/setup.sh
@@ -6,13 +6,21 @@ echo "execute migration files"
 
 
 function run_migrations() {
-  files="/usr/local/* "
+  files="/usr/local/migrations/* "
   for f in $files; do
     case $f in
       *.cql) cqlsh -f $f
     esac
   done
-  exit 1
+}
+
+function create_seed_data() {
+  files="/usr/local/seeds/* "
+  for f in $files; do
+    case $f in
+      *.cql) cqlsh -f $f
+    esac
+  done
 }
 
 function print_error() {
@@ -21,3 +29,6 @@ function print_error() {
 }
 
 run_migrations || print_error
+echo "migration is done!"
+create_seed_data || print_error
+echo "seed_data creation is done"
